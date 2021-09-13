f5c91a87fabc0183be123e0b5a6d5a9c07731a98
sanfusu <sanfusu@outlook.com>
2021-09-13 14:31:07 +0800

    移除多余内容

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 #![no_std]
 
-/// impl for writeable Regs;
-pub trait RegWrite<RegBufferType> {
-    fn write(buff: RegBufferType);
-}
-
 /// Flush the buffer into register;
 /// You can impl either RegWrite for Regs
 /// or RegBuffFlush for RegBufferType or both.
@@ -14,48 +9,33 @@ pub trait RegBufferFlush {
     /// rather than RegReadField::output.
     fn flush(&mut self);
 }
-/// Impl for RegBuffer::Regbuff if you want to config field.
-pub trait BufferWriter {
-    #[must_use = "The modified value works after flushed into register"]
-    fn write<T>(&mut self, value: T::ValueType) -> &mut Self
-    where
-        T: Field<Self> + FieldWriter<Self>,
-    {
-        T::write(self, value);
-        self
-    }
-}
-/// impl for RegBuffer::Regbuff if you want to get field;
-pub trait BufferReader {
-    fn read<T: Field<Self> + FieldReader<Self>>(&self) -> T::ValueType {
+pub trait RegisterBufferReader {
+    fn read<T: bits::field::Field<Self> + bits::field::FieldReader<Self>>(&self) -> T::ValueType {
         T::read(self)
     }
-    fn output<T: Field<Self> + FieldReader<Self>>(&self, out: &mut T::ValueType) -> &Self {
+    fn output<T: bits::field::Field<Self> + bits::field::FieldReader<Self>>(
+        &self,
+        out: &mut T::ValueType,
+    ) -> &Self {
         *out = T::read(self);
         self
     }
 }
-
-/// impl for Reg's fields;
-/// RegFieldWrite and RegFieldRead use the same ValueType and Regbuff to keep consistent.
-pub trait Field<RegBufferType>
-where
-    RegBufferType: ?Sized,
-{
-    type ValueType;
-}
-
-/// impl for RegField's instance
-pub trait FieldWriter<RegBufferType>: Field<RegBufferType>
-where
-    RegBufferType: ?Sized,
-{
-    fn write(reg_buff: &mut RegBufferType, value: Self::ValueType);
-}
-/// impl for RegField's instance
-pub trait FieldReader<RegBufferType>: Field<RegBufferType>
-where
-    RegBufferType: ?Sized,
-{
-    fn read(reg_buff: &RegBufferType) -> Self::ValueType;
+pub trait RegisterBufferWriter {
+    #[must_use = "The modified value works after flushed into register"]
+    fn write<T>(&mut self, value: T::ValueType) -> &mut Self
+    where
+        T: bits::field::Field<Self> + bits::field::FieldWriter<Self>,
+    {
+        T::write(self, value);
+        self
+    }
+    #[must_use = "The modified value works after flushed into register"]
+    fn revert<T>(&mut self) -> &mut Self
+    where
+        T: bits::field::Field<Self> + bits::field::FieldWriter<Self>,
+    {
+        T::revert(self);
+        self
+    }
 }
